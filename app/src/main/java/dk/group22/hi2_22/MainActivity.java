1a64c4ca2298c089fa84f0ebfd45937e7e9f40e5
Thomas Winding <tjfwinding@gmail.com>
2015-04-14 14:37:05 +0200

    Commit

---
+++ app/src/main/java/dk/group22/hi2_22/MainActivity.java
@@ -0,0 +1,95 @@
+package dk.group22.hi2_22;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    private static final String LOG_TAG = "HI2-22";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, getString(R.string.on_create));
+
+        /*TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("You are running Android " + Build.VERSION.SDK_INT);*/
+
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+
+        if (Build.VERSION.SDK_INT == 8) Toast.makeText(this, "Please, buy a new phone", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, getString(R.string.on_start));
+
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onStart();
+        Log.d(LOG_TAG, getString(R.string.on_resume));
+
+        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onStart();
+        Log.d(LOG_TAG, getString(R.string.on_pause));
+
+        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStart();
+        Log.d(LOG_TAG, getString(R.string.on_stop));
+
+        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, getString(R.string.on_destroy));
+
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
